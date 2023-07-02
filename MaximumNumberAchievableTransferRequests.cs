67d9c847563b02837da3ad02b69cff7f3523bbe5
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-02 15:47:38 +0300

    update read.me add solution

---
+++ MaximumNumberAchievableTransferRequests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumberAchievableTransferRequests
+{
+	internal class MaximumNumberAchievableTransferRequests
+	{
+		private int maximumRequests = 0;
+
+		private void MaximumRequests(int[][] requests, int[] indegree, int n, int index, int count)
+		{
+			if (index == requests.Length)
+			{
+				for (int i = 0; i < n; ++i)
+				{
+					if (indegree[i] != 0)
+					{
+						return;
+					}
+				}
+				maximumRequests = Math.Max(maximumRequests, count);
+				return;
+			}
+			indegree[requests[index][0]] -= 1;
+			indegree[requests[index][1]] += 1;
+			MaximumRequests(requests, indegree, n, index + 1, count + 1);
+			indegree[requests[index][0]] += 1;
+			indegree[requests[index][1]] -= 1;
+			MaximumRequests(requests, indegree, n, index + 1, count);
+		}
+
+		public int MaximumRequests(int n, int[][] requests)
+		{
+			maximumRequests = 0;
+			int[] dp = new int[n];
+			MaximumRequests(requests, dp, n, 0, 0);
+			return maximumRequests;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			MaximumNumberAchievableTransferRequests requests = new();
+            Console.WriteLine(requests.MaximumRequests(5, new int[][]
+			{
+				new int[] { 0, 1 },
+				new int[] { 1, 0 },
+				new int[] { 0, 1 },
+				new int[] { 1, 2 },
+				new int[] { 2, 0 },
+				new int[] { 3, 4 }
+			}));
+			Console.WriteLine(requests.MaximumRequests(3, new int[][]
+			{
+				new int[] { 0, 0 },
+				new int[] { 1, 2 },
+				new int[] { 2, 1 }
+			}));
+			Console.WriteLine(requests.MaximumRequests(4, new int[][]
+			{
+				new int[] { 0, 3 },
+				new int[] { 3, 1 },
+				new int[] { 1, 2 },
+				new int[] { 2, 0 }
+			}));
+			Console.WriteLine(requests.MaximumRequests(3, new int[][]
+			{
+				new int[] { 1, 2 },
+				new int[] { 1, 2 },
+				new int[] { 2, 2 },
+				new int[] { 0, 2 },
+				new int[] { 2, 1 },
+				new int[] { 1, 1 },
+				new int[] { 1, 2 }
+			}));
 		}
 	}
 }
